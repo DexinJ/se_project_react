ba9d2b9b4582433231bfb8b5703a7f6a3cc7ab2d
DexinJ <jindexin6@gmail.com>
2023-08-08 09:03:53 -0700

    renamed files

--- src/components/ModalWithForm/ModalWithForm.js
+++ src/components/ModalWithForm/ModalWithForm.js
@@ -11,11 +11,7 @@ function ModalWithForm({
     <div className={`modal modal_type_${name}`}>
       <div className="modal__container">
         <button type="button" onClick={onClose} className="modal__close">
-          <img
-            className="modal__close-image"
-            src="/images/Close.svg"
-            alt="close modal"
-          />
+          <image src="/images/Close.svg" alt="close modal" />
         </button>
         <h3 className="modal__text modal__heading">{title}</h3>
         <form className="modal__form">{children}</form>
